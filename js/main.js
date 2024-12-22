771aeeccd4ab511749d8c7aed3cb8c762dd53538
Habibur Rahman <hrsshainik@gmail.com>
2024-12-23 00:32:13 +0600

    up

--- js/main.js
+++ js/main.js
@@ -35,3 +35,25 @@ document.addEventListener("click", (ev) => {
         mobileSearch.classList.remove("search-active");
     }
 });
+
+
+//scrool to top//
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+  if (scrollPosition > totalHeight * 0.25) {
+    scrollToTopButton.classList.remove("hidden");
+  } else {
+    scrollToTopButton.classList.add("hidden");
+  }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+  });
+});
+//end//
